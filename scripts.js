13bb56db2c875f973c0826a771bb4ff3296e3594
kermit <kermit.liu1212@gmail.com>
2023-12-11 00:48:14 -0500

    Add computing functions

--- scripts.js
+++ scripts.js
@@ -1,9 +1,53 @@
 // creates element in the memory
 let expression = document.querySelector(".expression");
 let value = document.querySelector(".value");
-let number1 = 0;
-let number2 = 0;
+const allButtons = document.querySelectorAll('button')
+let num1 = [];
+let num2 = [];
+let equalsDown = false;
+let operatorDown = false;
 let operator = '';
+let statement = "";
+let result = "";
+
+
+// An event for all the buttons if needed 
+let addDigit = (array, digit) => array.push(digit);
+
+let removeDigit = (array) => array.pop();
+
+// calculate the total number given an array of digits
+let calculate = (numArr) => {
+    let total = 0;
+    let multipler = numArr.length - 1;
+    for (let i = 0; i < numArr.length; i++) {
+        total += numArr[i] * (10 ** multipler)
+        multipler--;
+    }
+    return total;
+};
+
+const digBut = document.querySelectorAll(".digit");
+digBut.forEach((but) => {
+    but.addEventListener('click', (e) => {
+        if (operatorDown) {
+            addDigit(num2, Number(e.target.value));
+        } else {
+            addDigit(num1, Number(e.target.value));
+        }
+
+        if (num2.length <= 0) {
+            operatorDown = true;
+        }
+
+        console.log(num1);
+        console.log(num2);
+
+        result += e.target.value;
+        value.textContent = result;
+    });
+});
+
 
 let add = (num1, num2) => {
     if (typeof num1 === 'string') {
@@ -13,16 +57,6 @@ let add = (num1, num2) => {
         num2 = Number(num2);
     }
     return num1 + num2;
-};
-
-
-
-let calculate = (num1, operator, num2) => {
-    if (operator === 'add') return `${num1} + ${num2} = ${add(num1, num2)}`;
-    if (operator === 'subtract') return `${num1} - ${num2} = ${subtract(num1, num2)}`;
-    if (operator === 'multiply') return `${num1} * ${num2} = ${multiply(num1, num2)}`;
-    if (operator === 'divide') return `${num1} / ${num2} = ${divide(num1, num2)}`;
-    if (operator === 'modulus') return `${num1} % ${num2} = ${mod(num1, num2)}`;
 };
 
 let subtract = (num1, num2) => {
@@ -55,7 +89,7 @@ let divide = (num1, num2) => {
     return num1 / num2;
 };
 
-let mod = (num1, num2) => {
+let modulus = (num1, num2) => {
     if (typeof num1 === 'string') {
         num1 = Number(num1);
     }
@@ -65,9 +99,85 @@ let mod = (num1, num2) => {
     return num1 % num2;
 };
 
+let operate = (num1, operator, num2) => {
+    if (operator === '+') return `${add(num1, num2)}`;
+    if (operator === '-') return `${subtract(num1, num2)}`;
+    if (operator === '×') return `${multiply(num1, num2)}`;
+    if (operator === '÷') return `${divide(num1, num2)}`;
+    if (operator === '%') return `${mod(num1, num2)}`;
+};
 
-expression.textContent = `${calculate(10, 'add', 50)}`;
-value.textContent = `${add(10, 50)}`;
+// event listener specifically for the equals button
+// this why I can use/see the previous operator
+const equalsButton = document.querySelector('#equals');
+equalsButton.addEventListener('click', (e) => {
+    operatorDown = false;
+    statement += result + (" " + e.target.value + " ");
+    console.log(statement);
+    expression.textContent = statement;
+    console.log(statement.split(' ')[1]);
+    result = operate(calculate(num1), statement.split(' ')[1], calculate(num2));
+    console.log(result);
+    value.textContent = result;
+});
+
+// event listener for the operations of the numbers
+const opButtons = document.querySelectorAll('.operator');
+opButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        operator = e.target.id;
+        console.log(operatorDown)
+        
+        operatorDown = true;
+        statement = result + (" " + e.target.value + " ");
+        console.log(statement);
+        result = removeAll(result);
+        expression.textContent = statement;
+    });
+});
+
+
+let removeAll = (expression) => {
+    expression = "";
+    return expression;
+};
+
+let removeOne = (expression) => {
+    const length = expression.length;
+    if (length < 1) {
+        return expression;
+    }
+    return expression.substring(0, length - 1);
+}
+
+// look to see if equals is pressed or if an operator is pressed
+// disable the operator and equals button
+const removeBut = document.querySelectorAll('.remove');
+removeBut.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.id === 'removeAll') {
+            statement = removeAll(statement);
+            expression.textContent = statement;
+            result = removeAll(result);
+            value.textContent = removeAll(result);
+            num1 = [];
+            num2 = [];
+            
+            console.table(num1);
+            console.table(num2);
+
+        }
+        if (e.target.id === 'removeOne') {
+
+            result = removeOne(result);
+            value.textContent = result;
+
+            console.table(num1);
+            console.table(num2);
+        }
+
+    });
+});
 
 
 
